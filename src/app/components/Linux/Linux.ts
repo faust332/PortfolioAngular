ebd86e0ed6a933347e179d183dc00c8898c97b3e
faust332 <129494376+faust332@users.noreply.github.com>
2026-02-28 22:10:02 +0100

    Integrate AgrandirComponent for image lightbox
    Refactor image handling by utilizing AgrandirComponent for lightbox functionality.

--- src/app/components/Linux/Linux.ts
+++ src/app/components/Linux/Linux.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NgFor, NgIf } from '@angular/common';
 import { ContactComponent } from '../contact/contact';
+import { AgrandirComponent } from '../agrandir/agrandir';
 
 @Component({
   selector: 'app-Linux',
   standalone: true,
-  imports: [RouterLink, NgFor, NgIf, ContactComponent],
+  imports: [RouterLink, NgFor, NgIf, ContactComponent, AgrandirComponent],
   templateUrl: './Linux.html',
   styleUrl: './Linux.css',
 })
 export class LinuxComponent {
+  @ViewChild('lb') lb!: AgrandirComponent;
   readonly base = 'images/linux';
 
   readonly images = [
@@ -19,23 +21,6 @@ export class LinuxComponent {
     'linux3.png',
   ].map((f) => `${this.base}/${f}`);
 
-  lightboxOpen = false;
-  lightboxImage: string | null = null;
-  
-  openImage(src: string) {
-  //console.log('OPEN', src);
-  this.lightboxImage = src;
-  this.lightboxOpen = true; 
-  }
-
-  closeImage() {
-    this.lightboxOpen = false;
-    this.lightboxImage = null;
-  }
-
-  // Debug
-  onImgError(ev: Event) {
-    const img = ev.target as HTMLImageElement;
-    console.log('Image not found:', img.src);
-  }
+  openImage(src: string) { this.lb.openImage(src); }
+  onImgError(ev: Event) { console.log('Image not found:', (ev.target as HTMLImageElement).src); }
 }
