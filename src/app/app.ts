25c39949046794f7baaf70dedb5adb69d95405d6
faust332 <129494376+faust332@users.noreply.github.com>
2026-02-28 22:25:39 +0100

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,25 +4,25 @@ import {
   ViewEncapsulation,
   inject,
   signal,
+  ViewChild
 } from '@angular/core';
 import { NgFor, NgIf } from '@angular/common';
 import { NavigationEnd, Router, RouterOutlet, RouterLink } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ContactComponent } from './components/contact/contact';
+import { AgrandirComponent } from './components/agrandir/agrandir';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, NgIf, ContactComponent],
+  imports: [RouterOutlet, RouterLink, NgIf, ContactComponent, AgrandirComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
   encapsulation: ViewEncapsulation.None,
 })
 export class App {
-  // Lightbox: l'image sélectionnée (null => fermé)
-  selectedImage = signal<string | null>(null);
-
+  @ViewChild('lb') lb!: AgrandirComponent;
   // Affichage page principale vs pages routées
   isMain = signal(true);
 
@@ -48,12 +48,5 @@ export class App {
   isMainPage() {
     return this.isMain();
   }
-
-  openImage(src: string) {
-    this.selectedImage.set(src);
-  }
-
-  closeImage() {
-    this.selectedImage.set(null);
-  }
+  openImage(src: string) { this.lb.openImage(src); }
 }
