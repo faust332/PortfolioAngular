dafc2632c800a7be8d23e4bde95d2c31525d985b
faust332 <129494376+faust332@users.noreply.github.com>
2026-02-28 21:41:19 +0100

    Refactor SingeComponent to use vaisseau images

--- src/app/components/Singe/Singe.ts
+++ src/app/components/Singe/Singe.ts
@@ -12,31 +12,12 @@ import { AgrandirComponent } from '../agrandir/agrandir';
   styleUrl: './Singe.css',
 })
 export class SingeComponent {
-  readonly base = 'images/singe';
+  @ViewChild('lb') lb!: AgrandirComponent;
 
-  readonly images = [
-    'singe1.png',
-    'singe2.png',
-    'singe3.png',
-  ].map((f) => `${this.base}/${f}`);
+  readonly base = 'images/vaisseau';
+  readonly images = ['vaisseau1.png', 'vaisseau2.avif', 'vaisseau3.avif']
+    .map((f) => `${this.base}/${f}`);
 
-  lightboxOpen = false;
-  lightboxImage: string | null = null;
-  
-  openImage(src: string) {
-    //console.log('OPEN', src);
-    this.lightboxImage = src;
-    this.lightboxOpen = true; 
-  }
-
-  closeImage() {
-    this.lightboxOpen = false;
-    this.lightboxImage = null;
-  }
-
-  // Debug
-  onImgError(ev: Event) {
-    const img = ev.target as HTMLImageElement;
-    console.log('Image not found:', img.src);
-  }
+  openImage(src: string) { this.lb.openImage(src); }
+  onImgError(ev: Event) { console.log('Image not found:', (ev.target as HTMLImageElement).src); }
 }
