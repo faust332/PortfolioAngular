25b246eb34211c5d975aa79cdf407e7d60f04131
Rayan ZOUAOUI <129494376+faust332@users.noreply.github.com>
2026-03-08 18:21:26 +0100

    Refactor main function to immediately invoked async function

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-async function main(): Promise<void> {
+(async () => {
   try {
     await bootstrapApplication(App, appConfig);
   } catch (err) {
     console.error(err);
   }
-}
-
-void main();
+})();
