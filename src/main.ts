b991695cee737451fbe5b8df95f9c74b5c2de1ff
Rayan ZOUAOUI <129494376+faust332@users.noreply.github.com>
2026-03-08 19:26:04 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,4 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-async function main(): Promise<void> {
-  try {
-    await bootstrapApplication(App, appConfig);
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-void main();
+await bootstrapApplication(App, appConfig).catch(console.error);
